3e4771e05874f595ae03381ee4fcbeb913aab072
hlomla <hlomla.tapuko@gmail.com>
2022-05-17 10:11:13 +0200

    added few more rows working on price

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ document.addEventListener('alpine:init', () => {
             count: 0,
             taxiAvail: 5,
             trips: 0,
+            price: 25,
+            count1: 0,
+            taxiAvail1: 5,
+            trips1: 0,
+            price1: 25,
+            count2: 0,
+            taxiAvail2: 5,
+            trips2: 0,
+            price2: 25,
 
             passengerCountUp(){
             return this.count++
@@ -26,7 +35,40 @@ document.addEventListener('alpine:init', () => {
                this.count = this.count - 5
                 this.taxiAvail =  this.taxiAvail- 1
                 return this.trips ++
-            },
+            }, passengerCountUpBell(){
+                return this.count1++
+                },
+                passengerCountDownBell(){
+                return this.count1--
+                },
+                taxisAvailBell(){
+                    return this.taxiAvail1
+                },
+                taxiTripsBell(){
+                    return this.trips1
+                },
+                leaveRankBell(){
+                   this.count1 = this.count1 - 5
+                    this.taxiAvail1 =  this.taxiAvail1- 1
+                    return this.trips1 ++
+                },
+                passengerCountUpK(){
+                    return this.count2++
+                    },
+                    passengerCountDownK(){
+                    return this.count2--
+                    },
+                    taxisAvailK(){
+                        return this.taxiAvail2
+                    },
+                    taxiTripsK(){
+                        return this.trips2
+                    },
+                    leaveRankK(){
+                       this.count2 = this.count2 - 5
+                        this.taxiAvail2 =  this.taxiAvail2- 1
+                        return this.trips2 ++
+                    },
             
         }
         
